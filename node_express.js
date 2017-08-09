36b6a0d46b76541e36ac1529ac722191196fdc6f
wuxiaohen <2750127294@qq.com>
2017-08-09 20:11:29 +0800

    node demo

---
+++ node_express.js
@@ -0,0 +1,23 @@
+'use strict';
+const express = require('express');
+const app = express();
+
+//以下是产生泄漏的代码
+let theThing = null;
+let replaceThing = function () {
+    let leak = theThing;
+    let unused = function () {
+        if (leak)
+            console.log("hi")
+    };
+    
+    // 不断修改theThing的引用
+    
+};
+
+app.get('/leak', function closureLeak(req, res, next) {
+    replaceThing();
+    res.send('Hllo Node');
+});
+
+app.listen(8082);
