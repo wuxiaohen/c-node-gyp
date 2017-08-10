3557f4f35fc19e4fd450b4c1916a290ab472c589
wuxiaohen <2750127294@qq.com>
2017-08-10 10:24:04 +0800

    EventEmitter this

--- test_events.js
+++ test_events.js
@@ -2,11 +2,19 @@ const EventEmitter = require('events')
 let emitter = new EventEmitter();
 //同步
 emitter.on('myEvent',() => {
-    console.log('hi 1');
+    console.log('hi 1',this);
 });
-emitter.on('myEvent',() => {
-    console.log('hi 2');
+// hi 1 {}
+emitter.on('myEvent',function() {
+    console.log('hi 2',this);
 });
+// hi 2 EventEmitter {
+//   domain: null,
+//   _events: { myEvent: [ [Function], [Function] ] },
+//   _eventsCount: 1,
+//   _maxListeners: undefined }
+
+
 //process  执行doSomething的时候不会等somethingComplicated执行完成才返回，
 //而是会立刻调用callback
 function somethingComplicated() {
@@ -30,9 +38,6 @@ doSomething(function () {
 //doing
 
 
-
-
-
 //Node.js 中执行 js 代码的过程是单线程的. 只有当前代码都执行完, 
 //才会切入事件循环, 然后从事件队列中 pop 出下一个回调函数开始执
 //行代码. 所以实现一个 sleep 函数, 只要通过一个死循环就可以阻塞整个 js 的执行流程.
@@ -40,7 +45,7 @@ function sleep(ms) {
   var start = Date.now(), expire = start + ms;
   while (Date.now() < expire) ;
   return;
-}; sleep(4000);
+}; sleep(2000);
 /*emitter.on('myEvent',() => {
     console.log('hi');
     emitter.emit('myEvent');
